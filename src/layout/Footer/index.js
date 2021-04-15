470ae3345ffbbd3f803ab7c11e1dda19482d8f75
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-15 17:45:30 +0100

    styling App

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './style.css';
 
-const Footer = () => <span id='footer'>Ravil Shafiyev</span>
+const Footer = () => {
+    return (
+        <>
+        <span id='footer'>Ravil Shafiyev</span>
+        <img id="pow" class="image-align" src="https://i.imgur.com/IFqIGsR.png" alt="POW!" />  
+        </>
+    );
+}
 
 export default Footer;
