cbb0a9e5bbca06b3e4488b8d2fd51001b59f45df
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-19 13:09:33 +0100

    adding NotFound page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <main aria-label="main" className="container">
+      <h1 className="pages-heading">Oops! Not Found!</h1>
+    </main>
+  )
+}
+
+export default NotFound;
