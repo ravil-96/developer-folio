0ba5d3a995821da054b5e7b24f3e1bae449f8569
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-19 10:29:49 +0100

    editing Quotes component to make it work (still doesn't work)

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -1,43 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import quotesData from './data.js'
 
 const Quotes = () => {
-    const [ quotes, setQuote ] = useState([
-        '"PERFECT IS THE ENEMY OF GOOD." –VOLTAIRE',
-        '"I’M STILL LEARNING." –MICHELANGELO',
-        '"LIFE IS A JOURNEY, NOT A DESTINATION." –RALPH WALDO EMERSON',
-        '"LEARNING IS NOT ATTAINED BY CHANCE, IT MUST BE SOUGHT FOR WITH ARDOR AND ATTENDED TO WITH DILIGENCE." ―ABIGAIL ADAMS',
-        '"YESTERDAY I WAS CLEVER, SO I CHANGED THE WORLD. TODAY I AM WISE, SO I AM CHANGING MYSELF." –RUMI',
-        '"BE CURIOUS, NOT JUDGMENTAL." –WALT WHITMAN',
-        '"YOU DON’T HAVE TO BE GREAT TO START, BUT YOU HAVE TO START TO BE GREAT." –ZIG ZIGLAR',
-        '"BE STUBBORN ABOUT YOUR GOALS AND FLEXIBLE ABOUT YOUR METHODS." –UNKNOWN',
-        '"NOTHING WILL WORK UNLESS YOU DO." –MAYA ANGELOU',
-        '"NEVER GIVE UP ON A DREAM JUST BECAUSE OF THE TIME IT WILL TAKE TO ACCOMPLISH IT. THE TIME WILL PASS ANYWAY." –EARL NIGHTINGALE',
-        '"ANYONE WHO STOPS LEARNING IS OLD, WHETHER AT TWENTY OR EIGHTY." —HENRY FORD',
-        '"TELL ME AND I FORGET. TEACH ME AND I REMEMBER. INVOLVE ME AND I LEARN." –BENJAMIN FRANKLIN',
-        '"CHANGE IS THE END RESULT OF ALL TRUE LEARNING." ―LEO BUSCAGLIA',
-        '"LIVE AS IF YOU WERE TO DIE TOMORROW. LEARN AS IF YOU WERE TO LIVE FOREVER." ―MAHATMA GANDHI',
-        '"A LEARNING CURVE IS ESSENTIAL TO GROWTH." –TAMMY BJELLAND'
-    ]);
+    const [ quotes, setQuotes ] = useState();
+
+    setQuotes(quotesData);
 
     useEffect(() => {
-        const index = () => {
+        
+        const renderQuotes = () => {
+            
             let index = Math.floor(Math.random() * quotes.length);
-            return index;
-        }
-        const renderQuote = () => {
-            return quotes[index]
-        }
-        renderQuote()
+                
+            return quotes.map(() => <tr><td>{quotes[index]}</td></tr>)
 
-        const stream = setInterval(renderQuote, 10000)
+        }
+        
+        stream = setInterval(renderQuotes, 10000)
 
-        return () => clearInterval(stream)
+        return () => clearInterval(stream) 
+        
     }, [])
 
     return (
         <div id="quotes-container">
         <h1 id="quotes-heading">Motivational Quotes</h1>
-        <p>{quotes}</p>
+        { quotes ? <p>{quotes}</p> : <h4>Quotes are on their way!</h4>}
         </div>
     )
 }
