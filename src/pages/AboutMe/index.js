a143bc10dd34e993669df2c4f94d4ef325a368ab
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-03 14:21:31 +0100

    adding scrollbar and unifying style

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -4,7 +4,7 @@ import { FeaturedStory, TruthsAndLies } from '../../components';
 const AboutMe = () => {
 
   return (
-      <>
+      <div class="scroll">
       <h2>About Me</h2>
       <p id="page-title">° A Dancer, a Programmer, a Walker and an Arts Enthusiast °</p>
         <div class="row">
@@ -13,12 +13,10 @@ const AboutMe = () => {
               <a href="https://youtu.be/YaN0EX2zZWc">
                 <img id="me-in-munich" src="https://i.imgur.com/qXVyHde.jpg" alt="Me in Budapest Városliget" />
               </a>
-              <aside>
-            <TruthsAndLies />
-          </aside>
-            </div>
+                <TruthsAndLies />
+              </div>
         </div>
-      </>
+      </div>
     );
 };
 
