841686e9d0d7a63aee219bbbbee4201a0dbf2a00
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-08 21:34:10 +0100

    add content and styling to contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,31 +1,24 @@
 import React from 'react';
+import './style.css'
 
 const Contact = () => {
     return (
-        <div>
+        <div class="icons-container">
               <ul id="social-icons">
                 <li>
-                    <a href="https://www.facebook.com/GitHub" data-ga-click="Footer, go to Facebook, text:facebook" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/facebook.svg" height="18" class="d-block" loading="lazy" decoding="async" alt="Facebook icon"/>
-                    <span class="sr-only">GitHub on Facebook</span>
+                    <a href="https://www.facebook.com/ravil.shafiyev/" class="color-gray-4">
+                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/facebook.svg" height="70" loading="lazy" decoding="async" alt="Facebook icon"/>
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.youtube.com/github" data-ga-click="Footer, go to YouTube, text:youtube" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/youtube.svg" height="16" class="d-block" loading="lazy" decoding="async" alt="YouTube icon" />
-                    <span class="sr-only">GitHub on YouTube</span>
+                    <a href="https://www.linkedin.com/in/ravil-shafiyev-408a5388" class="color-gray-4">
+                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/linkedin.svg" height="70" loading="lazy" decoding="async" alt="Linkedin icon" />
+                    
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/company/github" data-ga-click="Footer, go to Linkedin, text:linkedin" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/linkedin.svg" height="18" class="d-block" loading="lazy" decoding="async" alt="Linkedin icon" />
-                    <span class="sr-only">GitHub on LinkedIn</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/github" data-ga-click="Footer, go to github's org, text:github" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" height="20" class="d-block" loading="lazy" decoding="async" alt="GitHub mark" />
-                    <span class="sr-only">GitHub’s organization on GitHub</span>
+                    <a href="https://github.com/ravil-96" class="color-gray-4">
+                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" height="70" loading="lazy" decoding="async" alt="GitHub mark" />
                     </a>
                 </li>
             </ul>
