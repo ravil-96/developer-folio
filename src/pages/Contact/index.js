a5b83de1f939a129a8c7c93448d1488effcbe499
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-10 20:13:13 +0100

    style contact icons

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,18 +7,18 @@ const Contact = () => {
               <ul id="social-icons">
                 <li>
                     <a href="https://www.facebook.com/ravil.shafiyev/" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/facebook.svg" height="70" loading="lazy" decoding="async" alt="Facebook icon"/>
+                    <img id="icon" src="https://github.githubassets.com/images/modules/site/icons/footer/facebook.svg" height="100" loading="lazy" decoding="async" alt="Facebook icon"/>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/ravil-shafiyev-408a5388" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/linkedin.svg" height="70" loading="lazy" decoding="async" alt="Linkedin icon" />
+                    <img id="icon" src="https://github.githubassets.com/images/modules/site/icons/footer/linkedin.svg" height="100" loading="lazy" decoding="async" alt="Linkedin icon" />
                     
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/ravil-96" class="color-gray-4">
-                    <img src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" height="70" loading="lazy" decoding="async" alt="GitHub mark" />
+                    <img id="icon" src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" height="100" loading="lazy" decoding="async" alt="GitHub mark" />
                     </a>
                 </li>
             </ul>
