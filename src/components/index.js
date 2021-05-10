faad38cbe461096b11ca1033e89147d26fbefcd6
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-10 20:49:23 +0100

    cleaning code

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Quotes } from './Quotes';
-export { default as BackButton } from './BackButton';
 export { default as ProjectCards } from './ProjectCards';
 export { default as FeaturedStory } from './FeaturedStory';
 export { default as TruthsAndLies } from './TruthsAndLies';
