7aec9e238f45559ca373607290b9e7050536755d
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-10 19:59:46 +0100

    style portfolio button on home page

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -9,10 +9,10 @@ const Header = () => {
         <header>
         <img id="my-name" src="https://fontmeme.com/permalink/210503/e6a49f78a878b1f6e6d9b060deadd47b.png" alt="my_name" width="200"></img>
         <nav>
-        <NavLink exact to="/" activeClassName="current">Home</NavLink>
-        <NavLink to="/bio" activeClassName="current">Bio</NavLink>
-        <NavLink to="/projects" activeClassName="current">Projects</NavLink>
-        <NavLink to="/contact" activeClassName="current">Contact</NavLink>
+        <NavLink id="link" exact to="/" activeClassName="current">Home</NavLink>
+        <NavLink id="link" to="/bio" activeClassName="current">Bio</NavLink>
+        <NavLink id="link" to="/projects" activeClassName="current">Projects</NavLink>
+        <NavLink id="link"to="/contact" activeClassName="current">Contact</NavLink>
         </nav>
         </header>
         </>
