55de8906f6d276c4761fc4e86c8d6c1ece7c468d
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-10 20:48:08 +0100

    cleaning code

--- src/components/FeaturedStory/index.js
+++ src/components/FeaturedStory/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import BackButton from '../BackButton';
+import React from 'react';
 
 export default function FeaturedStory() {
     // const storiesData = [{id: 1, title: "About Me", body: "Tech savvy and an extroverted introvert, ordinary engineering graduate during day, dancer under cover of the night... As you can see, to describe myself as an individual is not an easy task! I am a person who finds inspiration in stories about travel and loves watching birds yet I am also an unstoppable city walker and dancing enthusiast listening to music most of my free time. How does that relate to coding, you would ask? Strangely but it all does come together in my mind. Physical sensations of colors, tastes, sounds and smells create excitation. After recharging my energies in quiet, I go out to take in London cityscapes either by walking or dancing. That inspires visions of life in me. Those visions make me realise possibilities for new designs yet to be realised - designs that can be silly and old or sometimes brave and bold. For most of the times, those designs require coding. And here I am, applying for software developer job and hoping that Futureproof will become my springboard into the future."},
@@ -10,7 +9,6 @@ export default function FeaturedStory() {
     
     return (
         <article aria-label="featured story" id="feature">
-            <BackButton />
             <p>
                 You can call me a developer, or an engineer, or even a dancer. Or you can simply call me a human. 
                 Latter describes me most accurately. However you prefer to call me, I do not fit well into any of the categories. 
